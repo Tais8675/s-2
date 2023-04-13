f278f33060227680c5cf0385344f316d73998073
Tais8675 <nascimentotaais@gmail.com>
2023-04-13 12:56:21 -0300

    terceiro commit

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import  app, lista, Compra
+from .models import  app, lista, Compra, ListaCompras
 
 
 admin.site.register(lista)
 #admin.site.register(app)
 admin.site.register(Compra)
+admin.site.register(ListaCompras)
+#admin.site.register(ListaComprasAdmin)
 
--- project/app/models.py
+++ project/app/models.py
@@ -23,3 +23,23 @@ class Compra(models.Model):
     
     def __str__(self):
         return f"ID:{self.id} Preço:{self.valor}  Produto:{self.comprar}"
+######
+
+class ListaCompras(models.Model):
+    nome = models.CharField(max_length=100)
+    item = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantidade} - {self.item} - R$ {self.valor}'
+
+    def total(self):
+        return self.quantidade * self.valor
+
+    total.short_description = 'total'
+
+#class ListaComprasAdmin(admin.ModelAdmin):
+ #   list_display = ('nome', 'item', 'quantidade', 'valor', 'total')
+  #  search_fields = ('nome', 'item')
+   # list_filter = ('nome',)
